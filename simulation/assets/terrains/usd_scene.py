d926ac6e80c795198aaf833ab2b4dee403246d61
liuzhi <18082258131@163.com>
2025-09-18 16:07:49 +0800

    update: move the ckpts, robots and terrains usd from assets to data directory

--- simulation/assets/terrains/usd_scene.py
+++ simulation/assets/terrains/usd_scene.py
@@ -9,7 +9,7 @@ ASSET_DICT = {
         prim_path=f"/World/Terrain",
         spawn=UsdFileCfg(
             visible=True,
-            usd_path=f"{SIMULATION_DATA_DIR}/usd/carla/carla.usd",
+            usd_path=f"{SIMULATION_DATA_DIR}/terrains/carla/carla.usd",
         ),
         init_state=AssetBaseCfg.InitialStateCfg(pos=(-200.0, -125.0, 0.0)),
     ),
@@ -17,7 +17,7 @@ ASSET_DICT = {
         prim_path=f"/World/Terrain",
         spawn=UsdFileCfg(
             visible=True,
-            usd_path=f"{SIMULATION_DATA_DIR}/usd/warehouse/warehouse_new.usd",
+            usd_path=f"{SIMULATION_DATA_DIR}/terrains/warehouse/warehouse_new.usd",
         ),
         init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, 0.0)),
     ),
@@ -25,7 +25,7 @@ ASSET_DICT = {
         prim_path=f"/World/Terrain",
         spawn=UsdFileCfg(
             visible=True,
-            usd_path=f"{SIMULATION_DATA_DIR}/usd/lobby/World_Lobby.usd",
+            usd_path=f"{SIMULATION_DATA_DIR}/terrains/lobby/World_Lobby.usd",
         ),
         init_state=AssetBaseCfg.InitialStateCfg(pos=(-20, -18, -1.0)),
     ),  # 
@@ -33,7 +33,7 @@ ASSET_DICT = {
         prim_path=f"/World/Terrain",
         spawn=UsdFileCfg(
             visible=True,
-            usd_path=f"{SIMULATION_DATA_DIR}/usd/Storage/WhiteHome01.usd",
+            usd_path=f"{SIMULATION_DATA_DIR}/terrains/Storage/WhiteHome01.usd",
         ),
         init_state=AssetBaseCfg.InitialStateCfg(pos=(0, 0, 0)),
     ),
