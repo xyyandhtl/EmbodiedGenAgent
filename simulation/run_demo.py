ce202356e7051bcf18da6180e443fd66dddb4105
liuzhi <18082258131@163.com>
2025-09-18 15:01:06 +0800

    update: add rgbd_camera sensor, and exchange Simulation with AppLauncher.app

--- simulation/run_demo.py
+++ simulation/run_demo.py
@@ -12,7 +12,7 @@ PROJECT_PATH = str(Path(__file__).resolve().parent.parent)
 sys.path.append(PROJECT_PATH)
 
 from simulation.assets import SIMULATION_DIR, SIMULATION_DATA_DIR
-from isaacsim import SimulationApp
+from isaaclab.app import AppLauncher
 import carb
 
 
@@ -21,18 +21,18 @@ def main(cfg):
     """Main function to run the Go2W locomotion demo."""
 
     # --- 1. Launch Omniverse App ---
-    simulation_app = SimulationApp({
-        "headless": cfg.sim_app.headless,
-        "anti_aliasing": cfg.sim_app.anti_aliasing,
-        "width": cfg.sim_app.width,
-        "height": cfg.sim_app.height,
-        "hide_ui": cfg.sim_app.hide_ui,
-        # "renderer": cfg.sim_app.renderer, # "RayTracedLighting"、"PathTracing"、"HydraStorm"
-    })
-    # for not setting
-    carb_settings_iface = carb.settings.get_settings()
-    carb_settings_iface.set("/persistent/isaac/asset_root/cloud",
-                           "https://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/Isaac/5.0")
+    simulation_app = AppLauncher(
+        headless=cfg.sim_app.headless,
+        enable_cameras=cfg.sim_app.record_video,  # if record_video=True, should set enable_cameras=True in simulation_app
+        anti_aliasing=cfg.sim_app.anti_aliasing,
+        width=cfg.sim_app.width,
+        height=cfg.sim_app.height,
+        hide_ui=cfg.sim_app.hide_ui,
+    ).app
+    # use SimulaitonApp directly should set carb_settings
+    # carb_settings_iface = carb.settings.get_settings()
+    # carb_settings_iface.set("/persistent/isaac/asset_root/cloud",
+    #                        "https://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/Isaac/5.0")
 
     from isaaclab.devices import Se2Keyboard, Se2KeyboardCfg
     from isaaclab.envs import ManagerBasedRLEnv
@@ -64,7 +64,7 @@ def main(cfg):
         print(controller)
         env_cfg.observations.policy.velocity_commands = ObsTerm(
             func=lambda env: torch.tensor(controller.advance(), dtype=torch.float32).unsqueeze(0).to(env.device)
-        )    
+        )
 
     # --- 3. Create Environment ---
     # The environment wrapper for Isaac Lab
@@ -117,6 +117,11 @@ def main(cfg):
             obs, _, _, _ = env.step(actions)
 
             # camera_follow(env, camera_offset_=(-2.0, -2.0, 1.0))
+
+            # print(f"[INFO] rgbd_camera data [rgb] shape: {env.unwrapped.scene['rgbd_camera'].data.output['rgb'].shape}")
+            # print(f"[INFO] rgbd_camera data [depth] shape: {env.unwrapped.scene['rgbd_camera'].data.output['distance_to_image_plane'].shape}")
+            # print(f"[INFO] rgbd_camera data [depth] max: {env.unwrapped.scene['rgbd_camera'].data.output['distance_to_image_plane'].max()} m, "
+            #       f"min: {env.unwrapped.scene['rgbd_camera'].data.output['distance_to_image_plane'].min()} m")
 
             # time delay for real-time evaluation
             elapsed_time = time.time() - start_time
--- simulation/utils/rsl_rl_utils.py
+++ simulation/utils/rsl_rl_utils.py
@@ -1,6 +1,8 @@
+import math
 import torch
 
 import isaaclab.utils.math as math_utils
+from isaaclab.sim.spawners.sensors.sensors_cfg import PinholeCameraCfg
 from isaaclab.envs import ManagerBasedRLEnv
 
 
@@ -45,3 +47,30 @@ def camera_follow(
         eye=smooth_camera_pos.cpu().numpy(),
         lookat=robot_pos.cpu().numpy()
     )
+
+
+def compute_cam_cfg(W=640, H=480, fov_deg_x=90.0):
+    # rgbd_camera settings
+    fov_deg_y = 2 * math.degrees(math.atan((H / W) * math.tan(math.radians(fov_deg_x) / 2.0)))
+
+    fx = W / (2.0 * math.tan(math.radians(fov_deg_x) / 2.0))
+    fy = H / (2.0 * math.tan(math.radians(fov_deg_y) / 2.0))
+    cx, cy = W / 2.0, H / 2.0
+    intrinsic_matrix = [fx, 0.0, cx, 0.0, fy, cy, 0.0, 0.0, 1.0]
+
+    cam_cfg = PinholeCameraCfg.from_intrinsic_matrix(
+        intrinsic_matrix=intrinsic_matrix,
+        width=W,
+        height=H,
+        clipping_range=(0.6, 100.0),  # 近、远平面，m
+        focal_length=12,  # 物理透视焦距，cm
+        f_stop=0.0,  # 光圈（F值），用于模拟景深
+    )
+    # PinholeCameraCfg 中的其他参数
+    # - horizontal_aperture、vertical_aperture 感光面尺寸，cm，在 USD/Omniverse 中是实际传感器的感光面尺寸的 10 倍
+    # - focus_distance 焦平面距离，m
+
+    print(f"[INFO] Computed fx={fx:.2f}, fy={fy:.2f}, HFOV={fov_deg_x:.2f} deg, VFOV={fov_deg_y:.2f} deg")
+    print(f"[INFO] Sensor width={cam_cfg.horizontal_aperture} cm, height={cam_cfg.vertical_aperture} cm")
+    print(f"[INFO] Physical focal length ≈ {cam_cfg.focal_length:.2f} cm")
+    return cam_cfg
