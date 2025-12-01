73dfe3e211dd8880a31d9b4cba70c650db781947
liulingkun <463422727@qq.com>
2025-12-01 14:13:11 +0800

    merge go2 camera and vel node

--- docs/scripts/tools.sh
+++ docs/scripts/tools.sh
@@ -2,4 +2,6 @@ convert Egent.png -quality 80 Agent.jpg
 
 ros2 bag record /camera/rgb/image_raw /camera/depth/image_raw /camera/pose /camera_info /lidar/pointcloud /lidar/pose -o isaaclab_city
 
+ros2 bag record /camera/rgb/image_raw /Odometry /livox/lidar -o go2w_1
+
 ros2 run image_view extract_images --ros-args -r image:=/udp_cam/image
